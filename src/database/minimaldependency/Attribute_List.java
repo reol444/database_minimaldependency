1dc52d95d4ff2a8051e4f6e36a830944af258067
muxin <870517035@qq.com>
2022-04-26 16:57:48 +0800

    update

--- src/database/minimaldependency/Attribute_List.java
+++ src/database/minimaldependency/Attribute_List.java
@@ -4,4 +4,7 @@ import java.util.ArrayList;
 
 public class Attribute_List {
     public ArrayList<Attribute> attributes;
+    public Attribute_List(){
+        attributes=new ArrayList<Attribute>();
+    }
 }
