455c54be546aba07780d3fb5f1f9770e7cbd95e5
muxin <870517035@qq.com>
2022-04-26 17:00:32 +0800

    Initial commit

--- src/database/minimaldependency/Attribute_List.java
+++ src/database/minimaldependency/Attribute_List.java
@@ -7,4 +7,16 @@ public class Attribute_List {
     public Attribute_List(){
         attributes=new ArrayList<Attribute>();
     }
+    public boolean is_equal(Attribute_List other) {
+        int length0 = this.attributes.size();
+        int length1 = other.attributes.size();
+        if (length0 != length1) return false;
+        for (int i = 0; i < length0; i++) {
+            Attribute a = this.attributes.get(i);
+            Attribute b = other.attributes.get(i);
+            if (!a.getName().equals(b.getName()))
+                return false;
+        }
+        return true;
+    }
 }
